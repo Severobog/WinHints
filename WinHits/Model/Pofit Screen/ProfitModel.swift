7d8b21677b9ceeb956e8b84c58d1626b557f67c4
Demid Starikov <severobog@gmail.com>
2022-07-24 22:37:36 +0500

    repair repository

---
+++ WinHits/Model/Pofit Screen/ProfitModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfitModel.swift
+//  WinHits
+//
+//  Created by Демид Стариков on 24.07.2022.
+//
+
+import Foundation
---
+++ WinHits/View/Betting Hints Screen/BettingHintsView.swift
@@ -0,0 +1,8 @@
+//
+//  BettingHintsView.swift
+//  WinHits
+//
+//  Created by Демид Стариков on 24.07.2022.
+//
+
+import Foundation
