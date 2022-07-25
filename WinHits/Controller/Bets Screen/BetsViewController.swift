74585f74e9de31206382e57c0c176b28731ec2d5
Demid Starikov <severobog@gmail.com>
2022-07-25 17:19:50 +0500

    fix BetsVC

--- WinHits/Controller/Bets Screen/BetsViewController.swift
+++ WinHits/Controller/Bets Screen/BetsViewController.swift
@@ -86,7 +86,7 @@ extension BetsViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top:  0, left: 0, bottom: 0, right: 0)
+        return UIEdgeInsets(top:  20, left: 0, bottom: 0, right: 0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
